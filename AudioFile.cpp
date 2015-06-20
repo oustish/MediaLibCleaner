8c728670b5938d4217b9fe4c972c65837a0c9c66
Szymon Oracki <oustish@gmail.com>
2015-06-20 12:11:23 +0200

    reorganized code and started adding doxygen comments

--- AudioFile.cpp
+++ AudioFile.cpp
@@ -1,6 +1,13 @@
-#include "AudioFile.h"
+#include "AudioFile.hpp"
 #include <memory>
 
+
+/**
+ * Constructor for MediaLibCleaner::AudioFile class.
+ * 
+ * AudioFile class constructor creaties instance of TagLib::FileRef object (or simillar) and reads all common tags from the file.
+ * @param[in] _path  Path to audio file this instance will represent
+ */
 MediaLibCleaner::AudioFile::AudioFile(std::string _path)
 {
 	this->pathname = _path;
@@ -33,54 +40,155 @@ MediaLibCleaner::AudioFile::~AudioFile() {
 
 }
 
+/**
+ * Method returns full path to audio file given object represents
+ *
+ * @return Path to audio file
+ */
 std::string MediaLibCleaner::AudioFile::GetPathname() {
 	return this->pathname;
 }
 
+/**
+ * Method allowing to read artist tag from an audio file
+ *
+ * @return Artist tag
+ */
 std::string MediaLibCleaner::AudioFile::GetArtist() {
 	return this->artist.toCString();
 }
 
+/**
+* Method allowing to read title tag from an audio file
+*
+* @return Title tag
+*/
 std::string MediaLibCleaner::AudioFile::GetTitle() {
 	return this->title.toCString();
 }
 
+/**
+* Method allowing to read album tag from an audio file
+*
+* @return Album tag
+*/
 std::string MediaLibCleaner::AudioFile::GetAlbum() {
 	return this->album.toCString();
 }
 
+/**
+* Method allowing to read genre tag from an audio file
+*
+* @return Genre tag
+*/
 std::string MediaLibCleaner::AudioFile::GetGenre() {
 	return this->genre.toCString();
 }
 
+/**
+* Method allowing to read comment tag from an audio file
+*
+* @return Comment tag
+*/
 std::string MediaLibCleaner::AudioFile::GetComment() {
 	return this->comment.toCString();
 }
 
+/**
+* Method allowing to read track tag from an audio file
+*
+* @return Track tag
+*/
 int MediaLibCleaner::AudioFile::GetTrack() {
 	return this->track;
 }
 
+/**
+* Method allowing to read year tag from an audio file
+*
+* @return Year tag
+*/
 int MediaLibCleaner::AudioFile::GetYear() {
 	return this->year;
 }
 
+/**
+* Method allowing to read bitrate of an audio file
+*
+* @return Bitrate (in kbps)
+*/
 int MediaLibCleaner::AudioFile::GetBitrate() {
 	return this->bitrate;
 }
 
+/**
+* Method allowing to read amount of channels in audio file
+*
+* @return Amount of channels
+*/
 int MediaLibCleaner::AudioFile::GetChannels() {
 	return this->channels;
 }
 
+/**
+* Method allowing to read audio file sample rate
+*
+* @return Sample rate (in Hz)
+*/
 int MediaLibCleaner::AudioFile::GetSampleRate() {
 	return this->sampleRate;
 }
 
+/**
+* Method allowing to read length of an audio file
+*
+* @return Bitrate in kbps
+*/
 int MediaLibCleaner::AudioFile::GetLength() {
 	return this->length;
 }
 
+/**
+* Method allowing to read status of given object
+*
+* @return Object status
+*/
 bool MediaLibCleaner::AudioFile::IsInitiated() {
 	return this->isInitiated;
+}
+
+/**
+* Method allowing to read length of an audio file as string
+*
+* @return Length of audio file in [[HH:]MM:]SS format
+*/
+std::string MediaLibCleaner::AudioFile::GetLengthAsString() {
+	std::string _out = "";
+	int hours = 0, minutes = 0, seconds = 0;
+
+	if (this->length >= 3600) { // if longer than or equal to 1 hour
+		hours = this->length / 3600; // no rest, only full hours
+
+		if (hours < 10) {
+			_out += "0";
+		}
+		_out += std::to_string(hours) + ":";
+	}
+	
+	if (this->length >= 60) { // if longer than or equal to 1 minute
+		minutes = (this->length - hours * 3600) / 60; //  no rest, only full remaining minutes
+
+		if (minutes < 10) {
+			_out += "0";
+		}
+		_out += std::to_string(minutes) + ":";
+	}
+
+	seconds = this->length - hours * 3600 - minutes * 60;
+	if (seconds < 10) {
+		_out += "0";
+	}
+	_out += std::to_string(seconds);
+
+	return _out;
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,241 @@
 #include <iostream>
 #include <memory> // for unique_ptr
-#include "AudioFile.h"
+/**
+@file
+@author Szymon Oracki <szymon.oracki@oustish.pl>
+@version 0.1
 
-int main(int argc, char *argv[]) {
-	std::unique_ptr <MediaLibCleaner::AudioFile> file(new MediaLibCleaner::AudioFile("D:\\Muzyka\\!Synced\\NEW!\\NEW! 2015.06\\Shinedown - Call Me.mp3"));
+@section DESCRIPTION
 
-	if (!file->IsInitiated()) return 1;
+This file is a start point for entire application.
+*/
 
-	std::cout << "FILE:       " << file->GetPathname() << std::endl;
-	std::cout << "ARTIST:     " << file->GetArtist() << std::endl;
-	std::cout << "TITLE:      " << file->GetTitle() << std::endl;
-	std::cout << "ALBUM:      " << file->GetAlbum() << std::endl;
-	std::cout << "TRACK:      " << file->GetTrack() << std::endl;
-	std::cout << std::endl;
-	std::cout << "BITRATE:    " << file->GetBitrate() << " [kbps]" << std::endl;
-	std::cout << "SAMPLERATE: " << file->GetSampleRate() << " [Hz]" << std::endl;
-	std::cout << "CHANNELS:   " << file->GetChannels() << std::endl;
-	std::cout << "LENGTH:     " << file->GetLength() << " [sec]" << std::endl;
+#include <fstream> // for file opening
+#include <streambuf> // for file opening
+#include <ctime> // for time()
 
-	system("pause > NUL");
+#include <lua/lua.hpp>
 
-	return 0;
+#include "LuaFunctions.hpp"
+
+
+
+
+void lua_error_reporting(lua_State*, int);
+void replaceAll(std::string&, const std::string&, const std::string&);
+static std::string _ReplaceAllAliasOccurences(std::string&, std::unique_ptr<MediaLibCleaner::AudioFile>&);
+
+
+std::string path = "D:\\Muzyka\\!Synced\\NEW!\\NEW! 2015.06\\Shinedown - Call Me.mp3";
+std::unique_ptr<MediaLibCleaner::AudioFile> audiofile;
+
+
+static int lua_caller_isaudiofile(lua_State *L) {
+	return lua_IsAudioFile(L, audiofile);
 }
+
+static int lua_caller_settags(lua_State *L) {
+	return lua_SetTags(L, audiofile);
+}
+
+
+
+
+// cerr and cout streams, if required by the user
+std::ofstream _cerr, _cout;
+
+// main function
+int main(int argc, char *argv[]) {
+
+	/*
+	 *
+	 * taglib test
+	 *
+	 **/
+	if (argc == 2) {
+		path = argv[1];
+	}
+
+	// init - debug
+	std::unique_ptr<MediaLibCleaner::AudioFile> audiofilet(new MediaLibCleaner::AudioFile(path));
+	audiofile.swap(audiofilet);
+	if (!audiofile->IsInitiated()) return 1;
+
+
+	/**
+	 *
+	 * READING LUA SETTINGS
+	 *
+	 **/
+	std::string _path = "", _error_log = "", _alert_log = "";
+	int _error_level = 0;
+
+	// read config file to memory
+	std::ifstream file("C:\\Users\\Szymon\\Documents\\test.lua");
+	std::string config((std::istreambuf_iterator<char>(file)),
+						std::istreambuf_iterator<char>());
+
+	// replace all occurences in string and return it as new string
+	std::string _newconfig = _ReplaceAllAliasOccurences(config, audiofile);
+
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	lua_register(L, "_IsAudioFile", lua_caller_isaudiofile);
+	lua_register(L, "_SetTags", lua_caller_settags);
+
+	// _action == System
+	// as we need these informations once at the beginning
+	int s = luaL_loadstring(L, _newconfig.c_str());
+	lua_pushstring(L, "System");
+	lua_setglobal(L, "_action");
+
+	// exetute script
+	if (s == 0) {
+		s = lua_pcall(L, 0, LUA_MULTRET, 0);
+	}
+	if (s == 0) { // because error code may change after execution
+		// read values to stack
+		// as this is stack - read it in reverse
+		// so returned values will be in order
+		lua_getglobal(L, "_error_level"); // -4
+		lua_getglobal(L, "_error_log"); // -3
+		lua_getglobal(L, "_alert_log"); // -2
+		lua_getglobal(L, "_path"); // -1
+
+		if (!lua_isstring(L, -1) || !lua_isstring(L, -2) || !lua_isstring(L, -3) || !lua_isnumber(L, -4)) {
+			return 2; // return error code here, so entire program will end; it's desired
+		}
+
+		// read all parameters
+		_path = lua_tostring(L, -1);
+		_alert_log = lua_tostring(L, -2);
+		_error_log = lua_tostring(L, -3);
+		_error_level = (int)lua_tonumber(L, -4);
+
+		if (_error_log != "-") {
+			// set cerr to proper value
+			_cerr.open(_error_log);
+			std::cerr.rdbuf(_cerr.rdbuf());
+		}
+
+		if (_alert_log != "-") {
+			// set cout to proper value
+			_cout.open(_alert_log);
+			std::cout.rdbuf(_cout.rdbuf());
+		}
+	}
+
+	std::cerr << "Testing" << std::endl;
+
+	lua_error_reporting(L, s);
+
+
+
+	
+}
+
+/**
+ * Function reporting errors of lua processor to std::cerr.
+ *
+ * This functions purpose is to check for errors after *.lua config file
+ * has been processed and, if present, print them to std::cerr.
+ * If error appeared after System initialization then
+ * std::cerr may be normal file defined by the user.
+ *
+ * @param[in] L       A lua_State object holding information about lua processor
+ * @param[in] status  Integer with status code returned after calling lua_pcall() or similar function
+ */
+void lua_error_reporting(lua_State *L, int status) {
+	lua_ErrorReporting(L, status);
+}
+
+/**
+ * Function replacing every occurence of one string with another in given string.
+ *
+ * This function allows to replace all occurences of one string with another string,
+ * all within another string.
+ *
+ * @param[in/out] str   String in which changes will be made. Please note argument is passed as referrence!
+ * @param[in]     from  String which will be replaced with another string
+ * @param[in]     to    String which will replace previous string
+ */
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+	if (from.empty())
+		return;
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+	}
+}
+
+static std::string _ReplaceAllAliasOccurences(std::string& _config, std::unique_ptr<MediaLibCleaner::AudioFile>& _audiofile) {
+	std::string _newc = _config;
+
+	// copy original path to not confuse rest of the program
+	std::string _path = _audiofile->GetPathname();
+	replaceAll(_path, "\\", "\\\\");
+
+	/*
+	remaining to implement:
+
+	%alumartist%, %bpm%, %copyright%, %language%, %length%, %mood%, %origalbum% ..., %unsyncedlyrics%, %www%
+	%_codec%, %_cover_mimetype%, %_cover_size%, %_cover_type%, %_covers%
+	%_ext%, %_filename%, %_filename_ext%, %_folderpath%, %_parent_dir%, %_volume%, %_workingdir%, %_workingpath%
+	%_file_{create,mod}_date%, %_file_{create,mod}_datetime%, %_file_{create,mod}_datetime_raw%,
+	%_file_size%, %file_size_bytes%, %_file_size_kb%, %_file_size_mb%
+	%_counter_dir%, %_counter_total%, %_date%, %_datetime%, %_total_files%, %_total_files_dir%
+	*/
+
+	// do the magic!
+	replaceAll(_newc, "%_path%", _path);
+	replaceAll(_newc, "%artist%", _audiofile->GetArtist());
+	replaceAll(_newc, "%title%", _audiofile->GetTitle());
+	replaceAll(_newc, "%album%", _audiofile->GetAlbum());
+	replaceAll(_newc, "%genre%", _audiofile->GetGenre());
+	replaceAll(_newc, "%comment%", _audiofile->GetComment());
+	replaceAll(_newc, "%track%", std::to_string(_audiofile->GetTrack()));
+	replaceAll(_newc, "%year%", std::to_string(_audiofile->GetYear()));
+
+	replaceAll(_newc, "%_bitrate%", std::to_string(_audiofile->GetBitrate()));
+	replaceAll(_newc, "%_channels%", std::to_string(_audiofile->GetChannels()));
+	replaceAll(_newc, "%_samplerate%", std::to_string(_audiofile->GetSampleRate()));
+	replaceAll(_newc, "%_length_seconds%", std::to_string(_audiofile->GetLength()));
+	replaceAll(_newc, "%_length%", _audiofile->GetLengthAsString());
+
+	replaceAll(_newc, "%_datetime_raw%", std::to_string(time(NULL)));
+
+	return _newc;
+}
+
+/*// evaluate rest of the file
+	s = luaL_loadstring(L, _newconfig.c_str());
+	lua_pushstring(L, ""); // make sure nothing is remaining in _action
+	lua_setglobal(L, "_action");
+
+	// execute script
+	if (s == 0) {
+		s = lua_pcall(L, 0, LUA_MULTRET, 0);
+	}
+	if (s == 0) {
+
+	}
+
+	lua_error_reporting(L, s);
+	lua_close(L);
+
+	// wait for user
+	system("pause >NUL");
+
+	// flush and close cout and cerr, if opened previously
+	if (_cerr.is_open()) {
+		_cerr.flush();
+		_cerr.close();
+	}
+
+	if (_cout.is_open()) {
+		_cout.flush();
+		_cout.close();
+	}
+
+	return 0;*/
