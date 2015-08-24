942ef3cb074c0dd2e65bf6b292cd55a8a997c679
Szymon Oracki <oustish@gmail.com>
2015-08-24 19:10:48 +0200

    added doxygen main page

--- main.hpp
+++ main.hpp
@@ -6,6 +6,27 @@
 This file is a start point header file for entire application.
 */
 
+/**
+@mainpage Documentation introduction
+
+@section intro_sec Introduction
+MediaLibCleaner is meant to be powerful tool helping everyone in continuous struggle to maintain large audio libraries by checking (and editing) tags, file names and folders structure to prevent chaos in the library
+
+Especially it will allow user to define multiple sets of rules for each (in)depended library folder, which includes (but is not limited to): checking, setting and deleting tags; moving, renaming and deleting any file (including non-audio files); checking files and folder integrity (files damage, unsupported content, tags content cohesion in directory etc.).
+
+Project is carried out by students of Gdansk University of Technology, Faculty of Electronics, Telecommunications and Informatics as part of the classes lead by Mulimedia Systems Department.
+
+Project documentation and other releated files are being kept at the server managed by the Department.
+
+@section used_sec Used libaries / credits
+This project is using these libraries:
+<ul>
+	<li><b>Boost</b> - licensed under the Boost Software License, Version 1.0,</li>
+	<li><b>Lua</b> - licensed under MIT license - property of PUC-Rio,</li>
+	<li><b>taglib</b> - licensed under LGPL and MPL.</li>
+</ul>
+*/
+
 #include <iostream>
 #include <memory> // for unique_ptr
 #include <fstream> // for file opening
